071b82835f0fb4c6e2c8b46467c417bdaa45a1d2
elther <elther@naver.com>
2022-05-05 22:20:41 +0900

    Initial commit

---
+++ src/test/java/hello/core/Springstudy001ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springstudy001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
